86f3bcf05c67bf6644a5eca270c3f98c18a9be64
nramnad <33083517+nramnad@users.noreply.github.com>
2022-03-29 21:08:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,8 +9,8 @@ terraform {
    }
 
 provider "aws" {
-       # region = "us-east-1" 
-        profile = "default"
+        region = "us-east-1" 
+        #profile = "default"
 	}
 
 resource "aws_instance" "Raminfra" {
