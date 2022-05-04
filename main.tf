cd2e2c07d97dd53af4b1572b4a3524a1f741a655
yogitk <100707703+yogitk@users.noreply.github.com>
2022-05-04 20:31:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ provider "aws" {
         #profile = "default"
 	}
 
-resource "aws_instance" "yogitakfra" {
+resource "aws_instance" "Raminfra" {
 	ami = "ami-0022f774911c1d690" 
 	instance_type = "t2.micro"
 }
