5e5bf1cef87b43d4f3d571f7fec182472091007f
yogitk <100707703+yogitk@users.noreply.github.com>
2022-05-04 10:41:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 terraform {
-    required_version = "~> 1.0.3" // added latest version
+    required_version = "~> 1.1.0" // added latest version
     required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "~> 4.12.1"
      }
     }
    }
@@ -14,6 +14,6 @@ provider "aws" {
 	}
 
 resource "aws_instance" "Raminfra" {
-	ami = "ami-0c2b8ca1dad447f8a" 
+	ami = "ami-0022f774911c1d690" 
 	instance_type = "t2.micro"
 }
